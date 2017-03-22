0a9fde181c72cd50bb1e3ab65194510710332830
Martijn Wiekens <martijn@tjoba.com>
2017-03-22 14:37:30 +0100

    Update main/window with JavaFX primaryStage init

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,9 +1,20 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.Window;
 
+public class Main extends Application {
+	Window window;
 	
 	public static void main(String args[]){
-		System.out.println("Start");
+		System.out.println("Starting ...");
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage){
+		this.window = new Window(this, primaryStage);
+		this.window.init();
 	}
 }
