2a477fb60d42e4fcd400b7eedc04311e982055b0
Martijn Wiekens <martijn@martijnwiekens.nl>
2017-03-28 14:16:41 +0200

    Update Main controller with createGame

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -20,4 +20,16 @@ public class Main extends Application {
 		this.window = new Window(this, primaryStage);
 		this.window.init();
 	}
+	
+	 /**
+     * Setup the connection and creates the game
+     * @param game the game of the game to be played
+     * @param ipadres Ipadres of game server
+     * @param portnumber Portnumber of gameserver
+     * @param player If player is a AI or Player
+     * @param playername The name of player
+     */
+	public void createGame(String game, String ipadres, String portnumber, String playertype, String playername){
+		return;
+	}
 }
