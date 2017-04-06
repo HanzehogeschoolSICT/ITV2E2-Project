8a589f6ca581e2fde7e6c9d1abe9aa8ea22693ab
Rutger Schroder <rutgerschroder94@gmail.com>
2017-04-06 13:38:44 +0200

    Added getGameStart() method to check if the game has started yet

--- src/controller/AbstractGame.java
+++ src/controller/AbstractGame.java
@@ -38,7 +38,10 @@ abstract class AbstractGame implements Game{
 	}
 	
 	public boolean getGameStart(){
-		return true;
+		 if(gameStatus == 1){
+		 	return true;
+		 }
+		 return false;
 	}
 	
 	public void setMove(int y, int x){
